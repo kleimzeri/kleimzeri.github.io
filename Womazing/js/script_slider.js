a48ca841e4f0ede11bae70855f7d79fcdbde471e
KLeimZeri <102779194+kleimzeri@users.noreply.github.com>
2022-04-13 01:28:41 +0300

    Add files via upload

---
+++ Womazing/js/script_slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $('.slider').slick({
+      dots:true,
+      autoplay:true,
+      autoplayspeed:3000,
+      speed:1500,
+   });
+});
---
+++ Womazing/js/script_slider_1.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider_12').slick({
+       arrows:true,
+       dots:true,
+       speed:1500,
+       infinite:false,
+    });
+ });
