a08e3184e5c2d67b898edbd617923cec44ba0797
KLeimZeri <102779194+kleimzeri@users.noreply.github.com>
2022-04-18 05:59:28 +0300

    старая не нужная версия

--- Womazing/js/script_slider.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $('.slider').slick({
-      dots:true,
-      autoplay:true,
-      autoplayspeed:3000,
-      speed:1500,
-   });
-});
--- Womazing/js/script_slider_1.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.slider_12').slick({
-       arrows:true,
-       dots:true,
-       speed:1500,
-       infinite:false,
-    });
- });
