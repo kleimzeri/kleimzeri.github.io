6d137e570ee3de4a8ffbf025e1acb646377c873c
KLeimZeri <102779194+kleimzeri@users.noreply.github.com>
2022-05-09 03:14:40 +0300

    Add files via upload

---
+++ 3.Kleimzeri/js/js_slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+       dots:true,
+       speed:1000,
+       centerMode:true,
+       autoplay:true,
+       waitforanimate:false,
+    });
+ });
