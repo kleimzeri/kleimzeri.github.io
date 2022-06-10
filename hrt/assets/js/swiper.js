7c7a33b01891c3d816a9836afc49b84636725bca
KLeimZeri <102779194+kleimzeri@users.noreply.github.com>
2022-06-10 19:05:47 +0300

    Add files via upload

---
+++ hrt/assets/js/swiper.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    Keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown:true,
+    },
+
+    slidesPerView: 1,
+    
+    spaceBetween: 50,
+
+    freeMode:true,
+
+//     // Navigation arrows
+//     navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+// If we need pagination
+pagination: {
+    el: '.swiper-pagination',
+  },
+
+    autoplay: {
+        delay:3000,
+        disableoninteraction: false,
+    },
+
+    speed: 900,
+  });
